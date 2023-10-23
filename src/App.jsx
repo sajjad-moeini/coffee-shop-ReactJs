50973b7b4e285dbc9a76fa8c72cad115734ba71d
sajjad <sajjadmoeini97@gmail.com>
2023-10-23 09:09:44 +0330

    add sections title box component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Router from './Router'
-import {useRoutes} from 'react-router-dom'
+import { useRoutes } from 'react-router-dom'
 import siteContext from './Context/Context'
 import './../node_modules/bootstrap/dist/css/bootstrap.css'
 function App() {
- const router = useRoutes(Router)
+  const router = useRoutes(Router)
 
   return (
     <>
-    <siteContext.Provider value={''}>
-{router}
-    </siteContext.Provider>
+      <siteContext.Provider value={''}>
+        {router}
+      </siteContext.Provider>
     </>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
 import HomeHeaderCarousel from '../../Components/HomeHeaderCarousel/HomeHeaderCarousel'
+import SectionTitle from '../../Components/SectionTitle/SectionTitle'
 export default function Home() {
   return (
    <>
    <HomeHeaderCarousel />
+   <SectionTitle subTitle={'What Happens Here'} title={'COFFEE BUILD YOUR BASE.'} titleColor={'black'} />
    </>
   )
 }
