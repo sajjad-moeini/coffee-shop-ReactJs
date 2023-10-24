8c3123d5fbd7b26bfbc24109c81ee4b5fc3f2cc0
sajjad <sajjadmoeini97@gmail.com>
2023-10-24 08:06:12 +0330

    update searchbar

--- src/Components/HeaderNav/HeaderNav.jsx
+++ src/Components/HeaderNav/HeaderNav.jsx
@@ -8,46 +8,58 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function HeaderNav() {
 
-const[bgColor,setBgColor]= useState('')
-const [navItemColor,setNavItemColor]=useState('white')
-const [navlogo,setNavLogo]=useState("images/home/logo-2.png")
+       const [bgColor, setBgColor] = useState('')
+       const [navItemColor, setNavItemColor] = useState('white')
+       const [navlogo, setNavLogo] = useState("images/home/logo-2.png")
+       const [isShowSearchBar, setIsShowSearchBar] = useState(false)
        window.document.addEventListener('scroll', () => {
               if (window.scrollY > 0) {
-                setBgColor('white')
-                setNavItemColor('#000')
-                setNavLogo("images/home/logo-1.png")
-               
-              } else {              
-                setBgColor('transparent')
-                setNavItemColor('#fff')   
-                setNavLogo("images/home/logo-2.png")          
+                     setBgColor('white')
+                     setNavItemColor('#000')
+                     setNavLogo("images/home/logo-1.png")
+
+              } else {
+                     setBgColor('transparent')
+                     setNavItemColor('#fff')
+                     setNavLogo("images/home/logo-2.png")
               }
-            })
-  return (
-    <>
-    <Navbar fixed="top" className='header-nav-container' expand="lg" style={{ background: bgColor }} >
-      <Container>
-        <Navbar.Brand href="#home">
-              <img src={navlogo} className='img-fluid logo'  alt="" />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto d-flex align-items-center justify-content-around w-75">
-            <NavLink to={'/'} style={{color:navItemColor}} className={(item)=>(item.isActive ? 'headerNav-item-active':'headerNav-item')}>Home</NavLink>
-            <NavLink to={'/menu'} style={{color:navItemColor}} className={(item)=>(item.isActive ? 'headerNav-item-active':'headerNav-item')}>Menu</NavLink>
-            <NavLink to={'/reservation'} style={{color:navItemColor}} className={(item)=>(item.isActive ? 'headerNav-item-active':'headerNav-item')}>Reservation</NavLink>
-            <NavLink to={'/account'} style={{color:navItemColor}} className={(item)=>(item.isActive ? 'headerNav-item-active':'headerNav-item')}>Account</NavLink>
-            <NavLink to={'/cart'} style={{color:navItemColor}} className={(item)=>(item.isActive ? 'headerNav-item-active':'headerNav-item')}>
-              <i className='fa fa-shopping-cart'></i>
-            </NavLink>
-            <Link  className={'headerNav-item'} style={{color:navItemColor}}>
-              <i className='fa fa-search'></i>
-            </Link>
-           
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </>
-  )
+       })
+       return (
+              <>
+                     <Navbar fixed="top" className='header-nav-container' expand="lg" style={{ background: bgColor }} >
+                            <div className="navbar-wrapper position-relative w-100 h-100 ">
+                                   <Container className=' d-flex'>
+                                          <Navbar.Brand href="#home">
+                                                 <img src={navlogo} className='img-fluid logo' alt="" />
+                                          </Navbar.Brand>
+                                          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                          <Navbar.Collapse id="basic-navbar-nav">
+                                                 <Nav className="ms-auto d-flex align-items-center justify-content-around w-75">
+                                                        <NavLink to={'/'} style={{ color: navItemColor }} className={(item) => (item.isActive ? 'headerNav-item-active' : 'headerNav-item')}>Home</NavLink>
+                                                        <NavLink to={'/menu'} style={{ color: navItemColor }} className={(item) => (item.isActive ? 'headerNav-item-active' : 'headerNav-item')}>Menu</NavLink>
+                                                        <NavLink to={'/reservation'} style={{ color: navItemColor }} className={(item) => (item.isActive ? 'headerNav-item-active' : 'headerNav-item')}>Reservation</NavLink>
+                                                        <NavLink to={'/account'} style={{ color: navItemColor }} className={(item) => (item.isActive ? 'headerNav-item-active' : 'headerNav-item')}>Account</NavLink>
+                                                        <NavLink to={'/cart'} style={{ color: navItemColor }} className={(item) => (item.isActive ? 'headerNav-item-active' : 'headerNav-item')}>
+                                                               <i className='fa fa-shopping-cart'></i>
+                                                        </NavLink>
+                                                        <Link className={'headerNav-item'} style={{ color: navItemColor }} onClick={()=>{setIsShowSearchBar(true)}}>
+                                                               <i className='fa fa-search'></i>
+                                                        </Link>
+                                                       {isShowSearchBar && <div className=' position-absolute searchbar-container bg-light'>
+                                                               <div className="row pt-3">
+                                                                      <div className="col-10 ">
+                                                                             <input type="text" className='form-control' placeholder='Search Here ...' />
+                                                                      </div>
+                                                                      <div className="col-2 d-flex-centering ">
+                                                                             <i className=' fa fa-remove searchbar-close-btn' onClick={()=>{setIsShowSearchBar(false)}}></i>
+                                                                      </div>
+                                                               </div>
+                                                        </div>} 
+                                                 </Nav>
+                                          </Navbar.Collapse>
+                                   </Container>
+                            </div>
+                     </Navbar>
+              </>
+       )
 }
